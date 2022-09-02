b297de2d5ca8a53223b39476fd246201bf6fb932
yu1ia <vasilenyuky@gmail.com>
2022-09-02 20:28:55 +0300

    Web Animations API

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+'use strict';
+
 console.log('Hello World!');
+
+const tail = document.querySelector('.tail');
+const chase = tail.animate([
+    {left: 'calc(50% - 80px)', top: 'calc(95vh / 2 - 50px)'},
+    {left: 'calc((50% - 80px) + 240px)', top: 'calc(95vh / 2 - 50px)'},
+    {left: 'calc((50% - 80px) + 240px)', top: 'calc((95vh / 2 - 50px) - 200px)'}, 
+    {left: 'calc(50% - 80px)', top: 'calc((95vh / 2 - 50px) - 200px)'},
+    {left: 'calc(50% - 80px)', top: 'calc(95vh / 2 - 50px)'}
+], {
+    duration: 53000,
+    iterations: 2,
+    fill: "forwards"
+});
